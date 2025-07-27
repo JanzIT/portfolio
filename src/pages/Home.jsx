c8315288d99d69e5cc74a55dbc1c17f77dcdcaf7
Renan Janz H. Vieira <janzhv.it@gmail.com>
2025-07-28 00:01:28 +0100

    feat(navbar): add scroll-aware nav bar with mobile menu toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
-import { StarBackground } from "../components/StarBackground"
-import { ThemeToggle } from "../components/ThemeToggle"
+import { Navbar } from "../components/Navbar";
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
 
-        { /* Theme Toggle */}
-        <ThemeToggle />
-        { /* Background Effects */}
-        <StarBackground />
-        { /* Navbar */}
-
-        { /* Main Content */}
-
-        { /* Footer */}
+      {/* Footer */}
     </div>
-}
+  );
+};
