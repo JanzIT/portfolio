299ee47239ed3eeacf144cc1ae7fa42f0dbad751
Renan Janz H. Vieira <janzhv.it@gmail.com>
2025-07-28 23:51:17 +0100

    feat(hero): add hero section with animated intro and scroll indicator

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import { ArrowDown } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section
+      id="Hero"
+      className=" relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className=" text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in"> Hi, I´m</span>
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              {" "}
+              Renan
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              Janz
+            </span>
+          </h1>
+          <p className="text-lg md:text-xl text-muted-forground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+            {" "}
+            I create stellar web experiences with modern technologies. As a
+            Fullstack Developer and Software Engineering student, I build
+            interfaces that are both beautiful and functional.
+          </p>
+          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-8 left-/12 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+        <span className=" text-sm text-muted-foreground mb-2"> Scroll </span>
+        <ArrowDown className="h-5 w-5 text-primary" />
+      </div>
+    </section>
+  );
+};
